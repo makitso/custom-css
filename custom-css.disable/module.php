2e7fe211a2a1d98a9c7587fcd3b71e1f9ebc8e7f
Rob Peters <makitso@gmail.com>
2022-06-07 08:57:48 -0700

    add module file

---
+++ custom-css.disable/module.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * Example theme.  Here we are extending an existing theme.
+ * Instead, you could extend AbstractModule and implement ModuleThemeInterface directly.
+ */
+
+declare(strict_types=1);
+
+namespace MyCustomNamespace;
+
+use Fisharebest\Webtrees\Module\AbstractModule;
+use Fisharebest\Webtrees\Module\ModuleCustomInterface;
+use Fisharebest\Webtrees\Module\ModuleCustomTrait;
+use Fisharebest\Webtrees\Module\ModuleGlobalInterface;
+use Fisharebest\Webtrees\Module\ModuleGlobalTrait;
+
+return new class extends AbstractModule implements ModuleCustomInterface, ModuleGlobalInterface {
+    use ModuleCustomTrait;
+    use ModuleGlobalTrait;
+
+    /**
+     * @return string
+     */
+    public function title(): string
+    {
+        return 'Theme modifications';
+    }
+
+    /**
+     * @return string
+     */
+    public function description(): string
+    {
+        return 'Theme modifications, as used on https://skatekey.net';
+    }
+
+    /**
+     * The person or organisation who created this module.
+     *
+     * @return string
+     */
+    public function customModuleAuthorName(): string
+    {
+        return 'Rob Peters';
+    }
+
+    /**
+     * The version of this module.
+     *
+     * @return string  e.g. '1.2.3'
+     */
+    public function customModuleVersion(): string
+    {
+        return '1.0.12';
+    }
+
+    /**
+     * A URL that will provide the latest version of this module.
+     *
+     * @return string
+     */
+    public function customModuleLatestVersionUrl(): string
+    {
+        return 'https://raw.githubusercontent.com/makitso/custom-css/master/latest-version.txt';
+    }
+
+    /**
+     * Where to get support for this module.  Perhaps a github respository?
+     *
+     * @return string
+     */
+    public function customModuleSupportUrl(): string
+    {
+        return 'https://github.com/makitso/custom-css/issues';
+    }
+
+    /**
+     * Where does this module store its resources
+     *
+     * @return string
+     */
+    public function resourcesFolder(): string
+    {
+        return __DIR__ . '/resources/';
+    }
+
+    /**
+     * Add an additional stylesheet to the header
+     *
+     * @return string
+     */
+    public function headContent(): string
+    {
+        $url = $this->assetUrl('css/custom.css');
+
+        return '<link rel="stylesheet" href="' . e($url) . '">';
+    }
+};
